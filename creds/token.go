44ca493fc655e2b7d5694190be2419925fede024
Rickard Andersson <gonz@severnatazvezda.com>
2020-10-28 08:58:37 +0200

    remove `Scope` from PK set in token

--- creds/token.go
+++ creds/token.go
@@ -7,7 +7,7 @@ import (
 
 type Token struct {
 	Id     uuid.UUID `json:"id" pg:"type:uuid,pk"`
-	Scope  string    `json:"scope" pg:",pk"`
+	Scope  string    `json:"scope"`
 	UserId uuid.UUID `json:"userId" pg:"type:uuid,notnull"`
 	User   *User     `json:"user" pg:"rel:has-one"`
 }

5dc19b6d6a79500202c0e4e463044529104fe508
Rickard Andersson <gonz@severnatazvezda.com>
2020-10-28 09:27:16 +0200

    move `initializeTestDatabase` to `database.go`

--- creds/database.go
+++ creds/database.go
@@ -2,9 +2,11 @@ package creds
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/go-pg/pg/v10"
 	"github.com/go-pg/pg/v10/orm"
+	"github.com/google/uuid"
 )
 
 func connectToDatabase(options DatabaseOptions) *pg.DB {
@@ -29,4 +31,44 @@ func createSchema(database *pg.DB, options *orm.CreateTableOptions) error {
 	}
 
 	return nil
+}
+
+type setUpData struct {
+	adminId    uuid.UUID
+	adminToken uuid.UUID
+	database   *pg.DB
+	adminScope string
+}
+
+func initializeTestDatabase() setUpData {
+	databaseOptions := DatabaseOptions{
+		Host:     GetRequiredEnvironmentVariable("TEST_DATABASE_HOST"),
+		Port:     GetRequiredEnvironmentIntegerEnvironmentVariable("TEST_DATABASE_PORT"),
+		Database: GetRequiredEnvironmentVariable("TEST_DATABASE_DATABASE"),
+		User:     GetRequiredEnvironmentVariable("TEST_DATABASE_USER"),
+		Password: GetRequiredEnvironmentVariable("TEST_DATABASE_PASSWORD"),
+	}
+
+	adminScope := GetRequiredEnvironmentVariable("TEST_ADMIN_SCOPE")
+	database := connectToDatabase(databaseOptions)
+	models := []interface{}{(*User)(nil), (*Token)(nil)}
+	for _, model := range models {
+		if err := database.Model(model).CreateTable(
+			&orm.CreateTableOptions{Temp: true, IfNotExists: true},
+		); err != nil {
+			log.Panicf("Unable to create test database: %s", err.Error())
+		}
+	}
+
+	adminId, err := insertUser(database, "Admin", "Admin")
+	if err != nil {
+		log.Panicf("Unable to create admin user: %s", err.Error())
+	}
+
+	adminToken, err := insertToken(database, adminId, adminScope)
+	if err != nil {
+		log.Panicf("Unable to create admin token: %s", err.Error())
+	}
+
+	return setUpData{adminId: adminId, adminToken: adminToken, database: database, adminScope: adminScope}
 }
